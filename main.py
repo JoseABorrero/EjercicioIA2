73a49fb662706e6da872094baa71954d1969b17f
Jose Antonio <jaborfer@gmail.com>
2024-10-17 19:32:40 +0200

    Versión comparar y buscar contrincante

--- main.py
+++ main.py
@@ -23,16 +23,27 @@ def generar_comparacion(pokemon1_info, pokemon2_info):
              f'2. {pokemon2_info['name']}: ' \
              f' Altura: {pokemon2_info['height']} decímetros ' \
              f' Peso: {pokemon2_info['weight']} hectogramos ' \
-             f' Tipos: {[tipo['type']['name'] for tipo in pokemon2_info['types']]}  ' 
-    
-    print ("Prompt enviado a ollama:")
-    print (prompt)         
+             f' Tipos: {[tipo['type']['name'] for tipo in pokemon2_info['types']]}  '          
     
     return prompt
 
-def usar_ollama(prompt):
+def usar_ollama_para_comparar(prompt):
      
     response = ollama.chat(model=MODELO_IA, messages=[
+  {
+    'role': 'user',
+    'content': prompt,
+  },
+])
+    return response['message']['content']
+
+def usar_ollama_para_buscar_contrario(prompt):
+     
+    response = ollama.chat(model=MODELO_IA, messages=[
+  {
+    'role': 'system',
+    'content': "Eres un entrenador pokemon,  busca el pokemon que sea fuerte contra hypno",
+  },
   {
     'role': 'user',
     'content': prompt,
@@ -46,10 +57,18 @@ def comparar_pokemons(pokemon1, pokemon2):
 
     if info1 and info2:
         prompt = generar_comparacion(info1, info2)
-        comparacion = usar_ollama(prompt)
+        print ("Prompt enviado a ollama para comparar sin contexto:")
+        print (prompt)
+        comparacion = usar_ollama_para_comparar(prompt)
         print(comparacion)
+        print("Contexto: Eres un entrenador pokemon,  busca el pokemon que sea fuerte contra hypno")
+        mejorcontrincante = usar_ollama_para_buscar_contrario(prompt)
+        print(mejorcontrincante)
+
+
 
 def main():
+    print("La aplicación pide 2 pokemons, primero los compara físicamente y luego te dice cual sería mejor en una lucha contra hypno")
     pokemon1 = input("Introduce el nombre del primer Pokémon: (Si no se escribe se tomará pikachu) ")
     if pokemon1 == "":
         pokemon1 = "pikachu"
